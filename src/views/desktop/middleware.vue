2ad267a12d7adbc7af213593830012011c4d6549
limEnough <limenough@github.com>
2023-12-03 22:16:15 +0900

    feat: add middleware file

---
+++ src/views/desktop/middleware.vue
@@ -0,0 +1,9 @@
+<template>
+  <component :is="currentComponent"></component>
+</template>
+
+<script setup lang="ts">
+  import middlewareComposable from '@/composables/views/middleware';
+
+  const { currentComponent } = middlewareComposable();
+</script>
