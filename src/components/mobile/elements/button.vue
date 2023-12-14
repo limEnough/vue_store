5cca040892c4dd75073cd15a4a4c764cc48f3b8e
limEnough <limenough@github.com>
2023-12-15 00:10:43 +0900

    feat: button 컴포넌트 생성

---
+++ src/components/mobile/elements/button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    :type="type"
+    :disabled="disabled"
+    :class="buttonClasses"
+    class="button-component"
+  >
+    <span
+      :class="[
+        {
+          [`icon--${props.icon}`]: props.icon, // icon
+          loading: props.loading, // loading
+        },
+      ]"
+      class="button-component__text"
+    >
+      <span>
+        <slot name="text"></slot>
+      </span>
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+  import buttonComposables, { buttonProps } from '@/composables/elements/button';
+
+  const props = defineProps(buttonProps);
+
+  const { buttonClasses } = buttonComposables(props);
+</script>
+
+<style lang="scss" scoped></style>
